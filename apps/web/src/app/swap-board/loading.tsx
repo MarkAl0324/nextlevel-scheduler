818bb57f91a7e4e4ac1c3cb2ffaabbe33cf53b57
Markal0324 <markalejandro1115@gmail.com>
2026-05-19 20:56:33 +0800

    auto: update loading.tsx

--- apps/web/src/app/swap-board/loading.tsx
+++ apps/web/src/app/swap-board/loading.tsx
@@ -6,14 +6,41 @@ export default function RequestsLoading() {
       <div className={styles.list}>
         {Array.from({ length: 4 }).map((_, i) => (
           <div key={i} className={styles.card}>
-            <div className={styles.cardLeft}>
-              <div className="nls-skeleton" style={{ width: 320, height: 14, marginBottom: 6 }} />
-              <div className="nls-skeleton" style={{ width: 260, height: 12 }} />
-              <div className="nls-skeleton" style={{ width: 220, height: 12, marginTop: 6 }} />
+            {/* Header skeleton: avatar + name + posted time */}
+            <div className={styles.cardHeader}>
+              <div
+                className="nls-skeleton"
+                style={{ width: 32, height: 32, borderRadius: "50%" }}
+              />
+              <div className={styles.cardHeaderText}>
+                <div
+                  className="nls-skeleton"
+                  style={{ width: 130, height: 13, marginBottom: 5 }}
+                />
+                <div className="nls-skeleton" style={{ width: 78, height: 11 }} />
+              </div>
             </div>
-            <div style={{ display: "flex", gap: 10, alignItems: "center" }}>
-              <div className="nls-skeleton" style={{ width: 72, height: 20, borderRadius: 999 }} />
-              <div className="nls-skeleton" style={{ width: 56, height: 14 }} />
+
+            {/* Shift block skeleton */}
+            <div className={styles.shiftBlock}>
+              <div className="nls-skeleton" style={{ width: 130, height: 10 }} />
+              <div className="nls-skeleton" style={{ width: 180, height: 18 }} />
+              <div className="nls-skeleton" style={{ width: 140, height: 12 }} />
+            </div>
+
+            {/* Reason skeleton */}
+            <div className="nls-skeleton" style={{ width: "70%", height: 12 }} />
+
+            {/* Footer skeleton: chip + button */}
+            <div className={styles.cardFooter}>
+              <div
+                className="nls-skeleton"
+                style={{ width: 64, height: 22, borderRadius: 999 }}
+              />
+              <div
+                className="nls-skeleton"
+                style={{ width: 90, height: 28, borderRadius: 8 }}
+              />
             </div>
           </div>
         ))}
